66989da74cafa496f3f2b052ca98f840eaa79160
Mara <mara-li@outlook.fr>
2022-04-14 19:58:27 +0200

    feat: Use glob to match excluded files

--- Snippet-downloader/utils.ts
+++ Snippet-downloader/utils.ts
@@ -1,15 +1,15 @@
 import {snippetDownloaderSettings} from "./settings";
-
+import globRegex from 'glob-regex';
 export function searchExcluded(settings: snippetDownloaderSettings, name: string): boolean {
-	const excluded = settings.excludedSnippet.split(",");
-	if (excluded.length <= 1) {
+	if (settings.excludedSnippet.length < 1) {
 		return false;
 	}
+	const excluded = settings.excludedSnippet.split(",");
 	for (const excl of excluded){
 		if (excl.trim() === name.replace('.css', '')){
 			return true;
 		}
-		const regExcl = new RegExp(excl.trim());
+		const regExcl = globRegex(excl.trim());
 		if (name.match(regExcl)){
 			return true;
 		}
