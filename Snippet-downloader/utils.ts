bec571581a968153a8424bfe393193d405bc0c6f
Mara <mara-li@outlook.fr>
2022-04-21 23:30:03 +0200

    fix: Adding regex in case of glob doest work
    BETA

--- Snippet-downloader/utils.ts
+++ Snippet-downloader/utils.ts
@@ -4,15 +4,19 @@ export function searchExcluded(excludedSnippet: string, name: string): boolean {
 	if (excludedSnippet.length < 1) {
 		return false;
 	}
-	const excluded = excludedSnippet.split(",");
+const excluded = test.split(",").map(e => {return e.trim();}).filter(x=>x.length>0)
 	for (const excl of excluded){
-		if (excl.trim() === name.replace('.css', '').trim()){
+		if (excl === name.replace('.css', '').trim()){
 			return true;
 		}
-		const regExcl = globRegex(excl.trim());
+		const regExcl = globRegex(excl);
 		if (name.match(regExcl)){
 			return true;
 		}
+		const regPureExcl= new RegExp(excl)
+		if (name.match(regPureExcl)) {
+			return true;
+		}	
 	}
 	return false;
 }
